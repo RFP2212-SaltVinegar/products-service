1bcb1279c9f6741ea3007888f22c56875d996fe5
Donna <donnatopia@gmail.com>
2023-02-20 14:21:12 -0800

    created query for the product styles

--- server/models.js
+++ server/models.js
@@ -3,7 +3,7 @@ const client = require('./db/connection.js');
 module.exports = {
   // for product
   getProduct: async (id) => {
-    const query = {
+    const product = {
       name: 'get-product',
       text: `SELECT json_build_object(
         'id', $1::INT,
@@ -12,70 +12,63 @@ module.exports = {
         'description', description,
         'category', category,
         'default_price', default_price,
-        'features', (SELECT json_agg(json_build_object(
-          'feature', feature,
-          'value', value
-        )) AS features FROM features WHERE product_id = $1::INT)
+        'features', (SELECT json_agg
+          (json_build_object(
+            'feature', feature,
+            'value', value
+          ))
+          FROM features
+          WHERE product_id = $1::INT
+        )
       )
+      AS data
       FROM products
       WHERE id = $1`,
       values: [id]
     }
 
     return await client
-      .query(query)
-      .then(({ rows }) => rows[0].json_build_object);
-
-    // var result = {};
-
-    // const product = {
-    //   text: `SELECT * FROM products WHERE id=$1`,
-    //   values: [id]
-    // };
-
-    // const features = {
-    //   text: `WITH features as (SELECT feature, value FROM features WHERE product_id = $1) SELECT json_agg(features.*) AS features FROM features`,
-    //   values: [id]
-    // }
-
-    // client
-    //   .query(product)
-    //   .then(({ rows }) => {
-    //     result = {...rows[0]}
-    //   })
-
-    // return client
-    //   .query(features)
-    //   .then(({ rows }) => {
-    //     result = {...result, ...rows[0]};
-    //     return result;
-    //   })
+      .query(product)
+      .then(({ rows }) => rows[0].data);
   },
 
   // for product styles
-  getProductStyles: (product_id) => {
+  getProductStyles: async (product_id) => {
     const styles = {
-      // text:
-      // `SELECT json_build_object(
-      //   'product_id', $1,
-      //   'results', (SELECT json_agg(
-      //     json_build_object(
-      //       'style_id', id,
-      //       'name', name,
-      //       'original_price', original_price,
-      //       'sale_price', sale_price,
-      //       'default?', default_price
-      //     )
-      //   ) FROM styles WHERE product_id  = $1
-      // )`,
-      // values: [product_id]
+      name: 'get-product-styles',
+      text: `SELECT json_build_object(
+        'product_id', $1::INT,
+        'results', (SELECT json_agg
+          (json_build_object(
+            'style_id', id,
+            'name', name,
+            'original_price', original_price,
+            'sale_price', sale_price,
+            'default?', default_style,
+            'photos', (SELECT json_agg
+              (json_build_object(
+                'thumbnail_url', thumbnail_url,
+                'url', url
+              )) FROM photos WHERE style_id = styles.id
+            ),
+            'skus', (SELECT json_object_agg(
+              skus.id, (SELECT json_build_object(
+                'quanitity', quantity,
+                'size', size
+              ))
+            ) FROM skus WHERE style_id = styles.id)
+          ))
+        )
+      )
+      AS data
+      FROM styles
+      WHERE product_id = $1`,
+      values: [product_id]
     };
 
-    return client
+    return await client
       .query(styles)
-      .then(({ rows }) => {
-        return rows;
-      });
+      .then(({ rows }) => rows[0].data);
   },
 
   // for related products
